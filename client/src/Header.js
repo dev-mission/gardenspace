330b9cec017260b73754c7138012684d0094d7e9
Jian Xie <jianxie@Jians-Air.attlocal.net>
2021-05-09 23:04:00 -0700

    Jian-FrontEnd

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,6 +3,8 @@ import {useHistory, Link} from 'react-router-dom';
 
 import Api from './Api';
 import {useAuthContext} from './AuthContext';
+import Logo from "./Logo.jpg";
+import './Header.scss';
 
 function Header() {
   const history = useHistory();
@@ -29,15 +31,29 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
       <div className="container">
-        <Link className="navbar-brand" to="/">My App</Link>
+        <div id="logo">
+          <img className="" src={Logo} alt="Logo"/>
+        </div>
+        <Link className="navbar-brand" to="/">GardenSpace</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>    
         <div className="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul className="navbar-nav flex-grow-1 mb-2 mb-md-0">
             <li className="nav-item active">
-              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/">Garden Locator</Link>
+              <Link className="nav-link" aria-current="page" to="/">What are community garden?</Link>
+              <Link className="nav-link" aria-current="page" to="/">Plant Library</Link>
+              <Link className="nav-link" aria-current="page" to="/">Get Involved</Link>
             </li>
+            <ul className="navbar-nav flex-grow-1 mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Owner Log in</Link>
+              </li>
+              <li className="nav-item">
+                  <Link className="nav-link" to="/login">Register My Garden</Link>
+                </li>
+            </ul>
             <div className="flex-grow-1 d-flex justify-content-end">
               {user && (
                 <li className="nav-item">
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,8 +1,52 @@
+import Gardenimg from "./gardenimg.jpg";
+
 function Home() {
   return (
-    <main className="container">
-      <h1>Home</h1>
-    </main>    
+    <div className="container">
+      <div>
+        <img className="" src={Gardenimg} alt="garden image"/>
+      </div>
+    <div className="">
+      <input className="" placeholder="Find a community garden near you" type="input"/>
+      <input type="submit"/>
+    </div>
+    <div>
+      <div className="container">
+        <div className="col-md-4">
+          <div></div>
+        </div>
+        <div className="col-md-8">
+            <h1>Garden</h1>
+            <p>Address</p>
+            <p>Type of Garden</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div className="container">
+        <div className="col-md-4">
+          <div></div>
+        </div>
+        <div className="col-md-8">
+            <h1>Garden</h1>
+            <p>Address</p>
+            <p>Type of Garden</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div className="container">
+        <div className="col-md-4">
+          <div></div>
+        </div>
+        <div className="col-md-8">
+            <h1>Garden</h1>
+            <p>Address</p>
+            <p>Type of Garden</p>
+        </div>
+      </div>
+    </div>
+  </div>  
   );
 }
 
