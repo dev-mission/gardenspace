f9eeae32ab4b234c6e152715c844fa416c32b92c
Jian Xie <jianxie@Jians-Air.attlocal.net>
2021-05-12 16:52:16 -0700

    Header stylization 90%

--- client/src/Header.js
+++ client/src/Header.js
@@ -31,42 +31,33 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
       <div className="container">
-        <div id="logo">
-          <img className="Logo" src={Logo} alt="Logo"/>
-        </div>
-        <Link className="navbar-brand" to="/">GardenSpace</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>    
-        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul className="navbar-nav flex-grow-1 mb-2 mb-md-0">
-            <li className="nav-item active">
-              <Link className="nav-link" aria-current="page" to="/">Garden Locator</Link>
-              <Link className="nav-link" aria-current="page" to="/">What are community garden?</Link>
-              <Link className="nav-link" aria-current="page" to="/">Plant Library</Link>
-              <Link className="nav-link" aria-current="page" to="/">Get Involved</Link>
-            </li>
-            <ul className="navbar-nav flex-grow-1 mb-2 mb-md-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Owner Log in</Link>
-              </li>
-              <li className="nav-item">
-                  <Link className="nav-link" to="/login">Register My Garden</Link>
+        <a class="navbar-brand" href="Home.js">
+          <img src={Logo} alt="Logo" width="38.95" height="50.53" class="d-inline-block align-text-top" />
+        </a>
+        <Link className="navbar-brand" to="Home.js" id="HeaderTitle">GardenSpace</Link>
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item" id="nav-item1">
+                  <Link className="nav-link" aria-current="page" to="/">Garden Locator</Link>
                 </li>
-            </ul>
-            <div className="flex-grow-1 d-flex justify-content-end">
-              {user && (
-                <li className="nav-item">
-                  <a className="nav-link" href="/logout" onClick={onLogout}>Log out</a>
-                </li>)}
-              {!user && (
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">Log in</Link>
+                <li class="nav-item">
+                <Link className="nav-link" aria-current="page" to="/">What are community garden?</Link>
                 </li>
-              )}
+                <li class="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/">Plant Library</Link>
+                </li>
+                <li class="nav-item">
+                  <Link className="nav-link" aria-current="page" to="/">Get Involved</Link>
+                </li>
+              </ul>
             </div>
-          </ul>
-        </div>
+          </div>
+          
+          <input type="button" class="btn btn-outline-dark" value="Owner Log In"/>
+          <div id="space"></div>
+          <input class="btn btn-success" type="button" value="Register My Garden"/>
+          
       </div>
     </nav>
   );
