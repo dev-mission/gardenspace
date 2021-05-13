9ffd7fabd70fa1dd13ec7cea03d8e2a6a3c78318
Jian Xie <jianxie@Jians-Air.attlocal.net>
2021-05-12 18:11:48 -0700

    Home.js stylization 95%

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,21 +3,26 @@ import './Home.scss';
 
 function Home() {
   return (
-    <div className="container">
+    <div className="container" id="body-container">
       <div className="col-md-6">
-        <img className="" src={Gardenimg} alt="garden image"/>
+        <img src={Gardenimg} class="img-thumbnail" alt="Garden Image"/>
       </div>
+
     <div className="col-md-6">
-    <div className="">
+      {/*<div className="">
       <input className="" placeholder="Find a community garden near you" type="input"/>
       <input type="submit"/>
-    </div>
+      </div>*/}
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Find a community garden near you" />
+        <button class="btn btn btn-success" type="button" id="button-addon2">Button</button>
+      </div>
     <div>
       <div className="container">
-        <div className="col-md-4">
-          <div></div>
+        <div className="col-md-4 mapBox">
+         
         </div>
-        <div className="col-md-8">
+        <div className="col-md-8 gardenInfo">
             <h1>Garden</h1>
             <p>Address</p>
             <p>Type of Garden</p>
@@ -26,10 +31,10 @@ function Home() {
     </div>
     <div>
       <div className="container">
-        <div className="col-md-4">
-          <div></div>
+        <div className="col-md-4 mapBox">
+         
         </div>
-        <div className="col-md-8">
+        <div className="col-md-8 gardenInfo">
             <h1>Garden</h1>
             <p>Address</p>
             <p>Type of Garden</p>
@@ -38,10 +43,10 @@ function Home() {
     </div>
     <div>
       <div className="container">
-        <div className="col-md-4">
-          <div></div>
+        <div className="col-md-4 mapBox">
+        
         </div>
-        <div className="col-md-8">
+        <div className="col-md-8 gardenInfo">
             <h1>Garden</h1>
             <p>Address</p>
             <p>Type of Garden</p>
